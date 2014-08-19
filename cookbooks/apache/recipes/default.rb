970a8763deba47abd20ecdc49e072cfccfb43fa9
Shawn Veader <shawn@veader.org>
2014-08-19 15:00:57 -0400

    lazy use of run_state

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,6 +15,19 @@ service "httpd" do
   action [ :enable, :start ]
 end
 
+ruby_block "randomly_choose_language" do
+  # run this block at execute time not compile time
+  block do
+    node.run_state["scripting_language"] = Random.rand > 0.5 ? "php" : "perl"
+  end
+end
+
+package "scripting_language" do
+  # use lazy to delay this to the execute phase
+  package_name lazy { node.run_state["scripting_language"] }
+  action :install
+end
+
 # Disable the default virtual host
 execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.disabled" do
   only_if do
