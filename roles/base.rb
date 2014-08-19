e4bcdcbffec484b97512c0e92f32371b7475334e
Shawn Veader <shawn@veader.org>
2014-08-19 11:28:24 -0400

    verify SSL

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,10 @@
 name "base"
 description "Base Server Role"
-run_list "recipe[chef-client::delete_validation]", "recipe[chef-client]", "recipe[ntp]", "recipe[motd]", "recipe[users]"
+run_list "recipe[chef-client::delete_validation]", "recipe[chef-client::config]", "recipe[chef-client]", "recipe[ntp]", "recipe[motd]", "recipe[users]"
+default_attributes(
+  "chef_client" => {
+    "config" => {
+      "ssl_verify_mode" => ":verify_peer"
+    }
+  }
+)
