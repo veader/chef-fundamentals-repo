4059bbc21f155962801eb21779d61be651bdb396
Shawn Veader <shawn@veader.org>
2014-08-20 09:35:29 -0400

    WIP

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -36,6 +36,7 @@ execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.disabl
   notifies :restart, "service[httpd]"
 end
 
+# Iterate over the apache sites
 node["apache"]["sites"].each do |site_name, site_data|
   apache_vhost site_name do
     site_port site_data["port"]
@@ -43,36 +44,3 @@ node["apache"]["sites"].each do |site_name, site_data|
     notifies :restart, "service[httpd]"
   end
 end
-
-# # Iterate over the apache sites
-# node["apache"]["sites"].each do |site_name, site_data|
-# # Set the document root
-#   document_root = "/srv/apache/#{site_name}"
-#
-# # Add a template for Apache virtual host configuration
-#   template "/etc/httpd/conf.d/#{site_name}.conf" do
-#     source "custom.erb"
-#     mode "0644"
-#     variables(
-#       :document_root => document_root,
-#       :port => site_data["port"]
-#     )
-#     notifies :restart, "service[httpd]"
-#   end
-#
-# # Add a directory resource to create the document_root
-#   directory document_root do
-#     mode "0755"
-#     recursive true
-#   end
-#
-# # Add a template resource for the virtual host's index.html
-#   template "#{document_root}/index.html" do
-#     source "index.html.erb"
-#     mode "0644"
-#     variables(
-#       :site_name => site_name,
-#       :port => site_data["port"]
-#     )
-#   end
-# end
